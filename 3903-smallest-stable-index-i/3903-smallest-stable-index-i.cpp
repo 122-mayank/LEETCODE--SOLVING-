cc1364a3d37afde357dbedf754eec238da0dccb3
Mayank Saini <mayanksaini20758@gmail.com>
2026-09-08 13:37:49 +0530

    Time: 5 ms (5.62%), Space: 31.6 MB (14.57%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
+        
+        vector<int>prefixMaximum(nums.size());
+        vector<int>prefixMinimum(nums.size());
 
-        //bruteforce 
+        int maxi = INT_MIN;
+        int mini = INT_MAX;
+
         int ans = INT_MAX;
-        for(int i = 0 ; i  < nums.size() ; i++){
-              int maxi = INT_MIN;
-              int mini = INT_MAX;
-              //find maximum
-              for(int j = 0 ; j <= i ; j++){
-                maxi = max(nums[j] , maxi);
-              }
-              //find minimum
-              for(int k = i ; k < nums.size() ; k++){
-                mini = min(nums[k] , mini);
-              }
-              int diff = maxi - mini;
 
-              if(diff <= k){
-                  ans = min(ans , i);
-              }
+        for(int i = 0 ; i < nums.size() ; i++){
+              maxi = max(maxi , nums[i]);
+              prefixMaximum[i] = maxi;
+        }
+
+        for(int j = nums.size() - 1 ; j >= 0 ; j--){
+             mini = min(mini , nums[j]);
+             prefixMinimum[j] = mini;
+        }
+
+        for(int i = 0 ; i < nums.size() ; i++){
+             
+             int diff = prefixMaximum[i] - prefixMinimum[i];
+
+             if(diff <= k){
+                ans = min(ans , i);
+             }
 
         }
 
         return ans == INT_MAX ? -1 : ans;
-        
+
     }
 };
